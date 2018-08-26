61495621174ec724ff2e1bb9be763a79ab1a9bca
Aleff Diniz <aleff_diniz@hotmail.com>
2018-08-26 18:52:32 -0300

    TESTANDO REST

---
+++ src/main/java/com/example/demo/CursomcApplication.java
@@ -0,0 +1,15 @@
+
+//para troca de porta
+//server.port=${port:8080}
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+}
