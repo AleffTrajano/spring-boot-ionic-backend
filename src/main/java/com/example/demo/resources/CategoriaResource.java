68c1302a3d1c84e6be3345605b7a12df709a997b
Aleff Diniz <aleff_diniz@hotmail.com>
2018-08-26 19:13:30 -0300

    rest tesado

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -4,13 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/* rest controller anotação para o rest precisa ser importada
+*estamos criando uma classe que vai ser um controlado rest
+* que vai responde ao endpoint categorias.
+*/
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	
-		@RequestMapping(method=RequestMethod.GET)
-		public String lista(){
-			return"rest esta funcionando";
-		}
+/*requestmapping verbo http para uma requisição basica get. */
+	@RequestMapping(method = RequestMethod.GET)
+	public String lista() {
+		return "rest esta funcionando";
+	}
 }
