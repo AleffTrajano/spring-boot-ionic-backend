49b25834056b3189fc53f99347ebbff850565bbb
Aleff Trajano <aleff_diniz@hotmail.com>
2018-09-11 11:06:13 -0300

    ajustando e testando a delecão de cliente

--- src/main/java/com/example/demo/resources/ClienteResource.java
+++ src/main/java/com/example/demo/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.example.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,8 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.demo.domain.Categoria;
 import com.example.demo.domain.Cliente;
+import com.example.demo.dto.ClienteNewDTO;
 import com.example.demo.dto.ClienteDTO;
 import com.example.demo.services.ClienteService;
 
@@ -38,12 +42,19 @@ public class ClienteResource {
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
-
 		Cliente obj = service.find(id);
-
 		return ResponseEntity.ok().body(obj);
-
 	}
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) throws IllegalAccessException {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+		path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+	
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
@@ -73,6 +84,7 @@ public class ClienteResource {
 
 	@RequestMapping(value = "/page", method = RequestMethod.GET)
 	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			
 			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
 			@RequestParam(value = "orderby", defaultValue = "nome") String ordeBy,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
--- src/main/java/com/example/demo/services/ClienteService.java
+++ src/main/java/com/example/demo/services/ClienteService.java
@@ -10,9 +10,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.domain.Cidade;
 import com.example.demo.domain.Cliente;
-import com.example.demo.domain.Cliente;
+import com.example.demo.domain.Endereco;
+import com.example.demo.domain.enums.TipoCliente;
 import com.example.demo.dto.ClienteDTO;
+import com.example.demo.dto.ClienteNewDTO;
 import com.example.demo.repositories.ClienteRepository;
 import com.example.demo.services.exceptions.ObjectNotFoundException;
 
@@ -33,6 +36,13 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	@org.springframework.transaction.annotation.Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+		
 	}
 
 	public Cliente update(Cliente obj) {
@@ -57,7 +67,6 @@ public class ClienteService {
 	}
 
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String ordeBy, String direction) {
-
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), ordeBy);
 		return repo.findAll(pageRequest);
 	}
@@ -66,6 +75,22 @@ public class ClienteService {
 		return new Cliente(objDto.getId(),objDto.getNome(),objDto.getEmail(),null,null);
 
 }
+
+	public Cliente fromDTO(ClienteNewDTO objDto) throws IllegalAccessException {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfouCnpj(),TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
+	
 	
 	private void updateData(Cliente newObj, Cliente obj){
 		
