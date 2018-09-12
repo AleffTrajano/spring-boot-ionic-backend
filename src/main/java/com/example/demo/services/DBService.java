af5d689bd2fbcd21f58a51b757e8e77ca46a5d29
Aleff Trajano <aleff_diniz@hotmail.com>
2018-09-12 18:24:23 -0300

    criando o profile de teste

---
+++ src/main/java/com/example/demo/services/DBService.java
@@ -0,0 +1,169 @@
+package com.example.demo.services;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Categoria;
+import com.example.demo.domain.Cidade;
+import com.example.demo.domain.Cliente;
+import com.example.demo.domain.Endereco;
+import com.example.demo.domain.Estado;
+import com.example.demo.domain.ItemPedido;
+import com.example.demo.domain.Pagamento;
+import com.example.demo.domain.PagamentoComBoleto;
+import com.example.demo.domain.PagamentoComCartao;
+import com.example.demo.domain.Pedido;
+import com.example.demo.domain.Produto;
+import com.example.demo.domain.enums.EstadoPagamento;
+import com.example.demo.domain.enums.TipoCliente;
+import com.example.demo.repositories.CategoriaRepository;
+import com.example.demo.repositories.CidadeRepository;
+import com.example.demo.repositories.ClienteRepository;
+import com.example.demo.repositories.EnderecosRepository;
+import com.example.demo.repositories.EstadoRepository;
+import com.example.demo.repositories.ItemPedidoRepository;
+import com.example.demo.repositories.PagamentoRepository;
+import com.example.demo.repositories.PedidoRepository;
+import com.example.demo.repositories.ProdutoRepository;
+
+@Service
+public class DBService {
+
+	// criando as dependecias;
+		@Autowired
+		private CategoriaRepository categoriaRepository;
+		@Autowired
+		private ProdutoRepository produtoRepository;
+		@Autowired
+		private EstadoRepository estadoRepository;
+		@Autowired
+		private CidadeRepository cidadeRepository;
+		@Autowired
+		private ClienteRepository clienteRepository;
+		@Autowired
+		private EnderecosRepository enderecoRepository;
+		@Autowired 
+		private PagamentoRepository pagamentoRepository;
+		@Autowired
+		private PedidoRepository pedidoRepository;
+		@Autowired
+		private ItemPedidoRepository itemPedidoRepository;
+	
+		public void instantiaTestDatabase() throws ParseException{
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "futebol");
+		Categoria cat4 = new Categoria(null, "eletronicos");
+		Categoria cat5 = new Categoria(null, "lazer");
+		Categoria cat6 = new Categoria(null, "decoração");
+		Categoria cat7 = new Categoria(null, "estilo de vida");
+				
+		Produto p1 = new Produto(null, "computador", 2000);
+		Produto p2 = new Produto(null, "impressora", 800);
+		Produto p3 = new Produto(null, "mouse", 80);
+		Produto p4 = new Produto(null, "mesa", 200);
+		Produto p5 = new Produto(null, "Toalha", 80);
+		Produto p6 = new Produto(null, "tv", 8000);
+		Produto p7 = new Produto(null, "chinelo", 20);
+		Produto p8 = new Produto(null, "celular", 350);
+		Produto p9 = new Produto(null, "antena", 30);
+		Produto p10 = new Produto(null, "conversor", 70);
+		Produto p11= new Produto(null, "lampada", 10);
+		
+		// COLOCANDO CADA PRODUTO COM A LISTA NA QUAL ELE INTERAGE
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2,p4));
+		cat3.getProdutos().addAll(Arrays.asList(p5,p6));
+		cat4.getProdutos().addAll(Arrays.asList(p1,p2,p3,p7));
+		cat5.getProdutos().addAll(Arrays.asList(p8));
+		cat6.getProdutos().addAll(Arrays.asList(p9,p10));
+		cat7.getProdutos().addAll(Arrays.asList(p11));
+		
+		
+		
+		
+		// FALANDO EM QUAL CATEGORIA O PRODUTO SE ENCAIXA
+		p1.getCategorias().addAll(Arrays.asList(cat1,cat4));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2,cat4));
+		p3.getCategorias().addAll(Arrays.asList(cat1,cat4));
+		p4.getCategorias().addAll(Arrays.asList(cat2));
+		p5.getCategorias().addAll(Arrays.asList(cat3));
+		p6.getCategorias().addAll(Arrays.asList(cat3));
+		p7.getCategorias().addAll(Arrays.asList(cat4));
+		p8.getCategorias().addAll(Arrays.asList(cat5));
+		p9.getCategorias().addAll(Arrays.asList(cat6));
+		p10.getCategorias().addAll(Arrays.asList(cat6));
+		p11.getCategorias().addAll(Arrays.asList(cat7));
+		// salvando esses objetos no banco
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2,cat3,cat4,cat5,cat6,cat7));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3,p4,p5,p6,p7,p8,p9,p10,p11));
+
+		
+		// criando as instancias de estado e cidade
+		Estado est1 = new Estado(null, "Minas gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberbelandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "campinas", est2);
+		// criando um lista de cidades que pertence ao estado 1 e 2
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		// salvando esses objetos no banco
+		estadoRepository.saveAll(Arrays.asList(est1 , est2));
+		cidadeRepository.saveAll(Arrays.asList(c1 , c2 , c3));
+		
+		Cliente cli1 =  new Cliente(null,"Aleff Mengo","aleffmengo@gmai.com","156165115664",TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("151561465","151564644864"));
+		Endereco e1 = new Endereco(null,"Rua joao","150","apt 12","jardim","326263",cli1,c1);
+		Endereco e2 = new Endereco(null,"Rua Maria","455","apt 13","mata","326326263",cli1,c2);
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+
+		///////////////////////////////////////////////////
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/08/2017 10:32"),cli1,e1);
+		Pedido ped2 = new Pedido(null,sdf.parse("10/10/2017 11:30") , cli1, e2);
+		
+		Pagamento pagt1 = new PagamentoComCartao(null,EstadoPagamento.QUITADO,ped1,6);
+		ped1.setPagamento(pagt1);
+		
+		
+		Pagamento pagt2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagt2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagt1, pagt2));
+		
+		
+		////////////////////////////////////////////////////
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p2, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p3, 0.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1,ip2,ip3));
+		
+		
+		};
+		
+	}
+
